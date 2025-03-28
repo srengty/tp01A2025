5d69975bb371020100463cd944e544a665a9f572
srengty <srengty@gmail.com>
2025-03-28 10:50:43 +0700

    update app java

--- src/main/java/itc/gic/i4a/App.java
+++ src/main/java/itc/gic/i4a/App.java
@@ -5,6 +5,6 @@ package itc.gic.i4a;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        system.out.println("Hello World!");
     }
 }
